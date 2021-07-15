cf3555627ed37706973971d61d92cadc2f4c2e1f
MathPSantos <matheus.psantos2016@gmail.com>
2021-07-15 08:49:20 -0300

    feat: add faua to project

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { query as q } from 'faunadb'
+
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -11,4 +15,25 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  jwt: {
+    signingKey: process.env.SIGNIN_KEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
